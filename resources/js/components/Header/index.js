f58335beab82554079869fb619dc75903df0a19f
Angelika Iskra <angela.iskra588@gmail.com>
2020-11-21 00:27:26 +0100

    finished header;

--- resources/js/components/Header/index.js
+++ resources/js/components/Header/index.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
+import PropTypes from 'prop-types';
 import Button from "../Button";
 import Search from "./Search";
+// import { slide as Menu } from 'react-burger-menu'
 
-const Header = () => {
+const Header = (props) => {
+
+    const isLoggedIn = false;
+    const [category, setCategory] = useState("");
+    const [inputVal, setInputVal] = useState("");
+
     return (
-        <div className={"header"}>
-            <div className={"header__container"}>
+        <Fragment>
+        <div className={"header"} id={"page-wrap"}>
+            <div className={"header__container container"}>
 
-                <span className={"logo header__logo"}/>
-                <Search />
-                <Button text={"Dołącz do nas!"} isGreen={true}/>
+                <span className={"logo header__logo"}>
+                    {/*<img src={"./assets/images/logo/lokando-logo-white.svg"} alt={"Logo"} />*/}
+                </span>
+
+                <Search
+                    onSearch={() => props.onSearch(inputVal, category)}
+                    setInputVal={(val) => setInputVal(val)}
+                    setCategory={(category) => setCategory(category)}
+                    inputVal={inputVal}
+                    category={category}
+                />
+
+                {isLoggedIn
+                    ?
+                    null // burger
+                    :
+                    <Button text={"Dołącz do nas!"} isGreen={true}/>
+                }
 
             </div>
         </div>
+            <Search
+                mobile={true}
+                onSearch={() => props.onSearch(inputVal, category)}
+                setInputVal={(val) => setInputVal(val)}
+                setCategory={(category) => setCategory(category)}
+                inputVal={inputVal}
+                category={category}
+            />
+        </Fragment>
     );
+};
+
+Header.propTypes = {
+    onSearch: PropTypes.func,
 };
 
 export default Header;
--- resources/js/views/Home/index.js
+++ resources/js/views/Home/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import Header from "../../components/Header";
 
 const Home = () => {
+
+    const onSearch = (inputText, category) => {
+      console.log(inputText, category);
+    };
+
     return (
         <div>
-            <Header/>
+            <Header onSearch={(inputText, category) => onSearch(inputText, category)} />
             <h1>Home</h1>
         </div>
     );
