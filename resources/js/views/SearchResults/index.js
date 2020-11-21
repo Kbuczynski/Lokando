c66084be617a4059ef641f0bb91f439b176f2c08
Angelika Iskra <angela.iskra588@gmail.com>
2020-11-21 04:08:23 +0100

    worked on results screen;

--- resources/js/views/SearchResults/index.js
+++ resources/js/views/SearchResults/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Wrapper from "../../components/Wrapper";
 import Filters from "./Filters";
+import Product from "../../components/Product";
+import PropTypes from "prop-types";
 
 const SearchResults = (props) => {
+
+    const [category, setCategory] = useState("");
+    const [priceRange, setPriceRange] = useState([0, 1000]);
+    const [status, setStatus] = useState("");
+    const [sort, setSort] = useState("domyslnie");
+
     return (
         <Wrapper>
-            <Filters />
+            <Filters
+                category={category}
+                setCategory={setCategory}
+                priceRange={priceRange}
+                setPriceRange={setPriceRange}
+                status={status}
+                setStatus={setStatus}
+                sort={sort}
+                setSort={setSort}
+            />
+
+            <div className={"searchResults__products products container"}>
+                <Product />
+            </div>
+
         </Wrapper>
     );
 };
