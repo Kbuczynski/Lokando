7649d2c252cfe341ead78666e59405075addd71b
Kbuczynski <kamil.buczynski00@interia.pl>
2020-11-21 01:04:55 +0100

    create home page

--- resources/js/views/Home/index.js
+++ resources/js/views/Home/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "../../components/Header";
+import Category from "./Category";
+import HomeSlider from "./HomeSlider";
+import NewCompanies from "./NewCompanies";
 
 const Home = () => {
     return (
         <div>
-            <Header/>
-            <h1>Home</h1>
+            <Header />
+            <div className="container">
+                <HomeSlider />
+                <Category />
+            </div>
+            <NewCompanies />
         </div>
     );
 };
